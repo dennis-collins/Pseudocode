0604471ca4a105bc0c2e2938fdf2f2b2700200ae
Dennis Collins <111964519+dennis-collins@users.noreply.github.com>
2024-12-20 20:02:24 +0100

    Updated script.js med pseudocode för VG-uppgiften.

--- script.js
+++ script.js
@@ -31,15 +31,51 @@ play();
 function play()
     SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
     SET variabel startOrd till "FOUR";
-    SET variabel slutordOrd till "FIVE";
+    SET variabel slutOrd till "FIVE";
+    SET variabel wordOne till "FOUR";
+    SET variabel wordTwo till "0"; // som placeholder i början
+    SET variabel plays = 0;
+    SET variabel congrats till "Well done! You did it in [plays] steps.";
 
+    PRINT prompt to user to enter a word
+    
+    INPUT wordTwo
+    INCREASE plays ++
+
+    IF ordbok contains wordTWO THEN
+        RUN function isOneLetterApart()
+    ELSE
+        PRINT initial prompt to user again
+    END IF
+
+    IF function isOneLetterApart() RETURNS true THEN
+        ASSIGN value of wordTwo to wordOne
+    ELSE
+        PRINT initial prompt to user again
+    END IF
+
+    IF wordOne == slutOrd THEN
+        PRINT "congrats" to user
+    ELSE
+        PRINT initial prompt to user again
+    END IF
 
 end function
 
 function isOneLetterApart(wordOne, wordTwo)
     SET variabel diffCount till 0;
 
-    // Här behöver du skriva koden för din funktion
+    LOOP through 0 to 3 for i
+        IF wordOne [i] != wordTwo [i]
+            THEN diffCount ++
+        END IF
+    END LOOP
+
+    IF diffCount == 1 THEN
+        RETURN true
+    ELSE
+        RETURN false
+    END IF
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
 end function
